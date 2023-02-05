0d92281113cb05a34166b323a4c9bd8a6b42a9ea
Schofield, stuart <546wine@gmail.com>
2023-02-05 14:33:44 -0700

    Moved everything out to MainView instead of MainWindow... it still isn't working but...

--- StudentsList/Views/MainView.axaml.cs
+++ StudentsList/Views/MainView.axaml.cs
@@ -1,12 +1,61 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.Threading;
+using StudentsList.ViewModels;
+using System;
 
 namespace StudentsList.Views
 {
     public partial class MainView : UserControl
     {
+
+        #region Private Members
+
+        private Control mMainGrid;
+        private Control mAddNewStudentButton;
+        private Control mAddNewStudentPopup;
+
+        #endregion
+
+
         public MainView()
         {
             InitializeComponent();
+
+            DataContext = new MainViewModel();
+
+            // Gather the named controls
+            mAddNewStudentButton = this.FindControl<Control>("AddNewStudentButton") ?? throw new Exception("Cannot find AddNewStudentButton by name");
+            mAddNewStudentPopup = this.FindControl<Control>("AddNewStudentPopup") ?? throw new Exception("Cannot find AddNewStudentPopup by name");
+            mMainGrid = this.FindControl<Control>("MainGrid") ?? throw new Exception("Cannot find Main Grid by name");
         }
+
+        public override void Render(DrawingContext context)
+        {
+            base.Render(context);
+
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                // Get relative position of button, in relation to main grid
+                var position = mAddNewStudentButton.TranslatePoint(new Point(), mMainGrid) ??
+                               throw new Exception("Cannot get TranslatePoint from Configuration Button");
+
+                // Set margin of popup so it appears bottom left of button
+                mAddNewStudentPopup.Margin = new Thickness(
+                    position.X,
+                    0,
+                    0,
+                    mMainGrid.Bounds.Height - position.Y - mAddNewStudentButton.Bounds.Height);
+            });
+        }
+
+        private void InputElement_OnPointerPressed(object sender, PointerPressedEventArgs e)
+        {
+            ((MainViewModel)DataContext).AddNewStudentButtonPressedCommand.Execute(null);
+        }
+
+
     }
 }
--- StudentsList/Views/MainWindow.axaml.cs
+++ StudentsList/Views/MainWindow.axaml.cs
@@ -17,13 +17,7 @@ namespace StudentsList.Views;
 public partial class MainWindow : Window 
 
 {
-    #region Private Members
 
-    private Control mMainGrid;
-    private Control mAddNewStudentButton;
-    private Control mAddNewStudentPopup;
-
-    #endregion
 
 
     #region Constructors
@@ -36,36 +30,11 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
 
-        // Gather the named controls
-        mAddNewStudentButton = this.FindControl<Control>("AddNewStudentButton") ?? throw new Exception("Cannot find AddNewStudentButton by name");
-        mAddNewStudentPopup = this.FindControl<Control>("AddNewStudentPopup") ?? throw new Exception("Cannot find AddNewStudentPopup by name");
-        mMainGrid = this.FindControl<Control>("MainGrid") ?? throw new Exception("Cannot find Main Grid by name");
+       
     }
 
     #endregion
 
-    public override void Render(DrawingContext context)
-    {
-        base.Render(context);
-
-        Dispatcher.UIThread.InvokeAsync(() =>
-        {
-            // Get relative position of button, in relation to main grid
-            var position = mAddNewStudentButton.TranslatePoint(new Point(), mMainGrid) ??
-                           throw new Exception("Cannot get TranslatePoint from Configuration Button");
-
-            // Set margin of popup so it appears bottom left of button
-            mAddNewStudentPopup.Margin = new Thickness(
-                position.X,
-                0,
-                0,
-                mMainGrid.Bounds.Height - position.Y - mAddNewStudentButton.Bounds.Height);
-        });
-    }
-
-    private void InputElement_OnPointerPressed(object sender, PointerPressedEventArgs e)
-    {
-        ((MainViewModel)DataContext).AddNewStudentButtonPressedCommand.Execute(null);
-    }
+   
          
 }
