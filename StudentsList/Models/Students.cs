dcab31d4f4ecd54bfaae12fc5dd4b41cf52dd858
Schofield, stuart <546wine@gmail.com>
2023-02-04 21:06:25 -0700

    Added ShowDelete to Student model, to show the delete button when the student is selected in the listbox changed ItemsControl to Listbox Added a bunch of comments re-organized the code in the ViewModel and surrounded the different parts with Regions It Still isn't working yet, I will try to create a StudentList model next, I guess

--- StudentsList/Models/Students.cs
+++ StudentsList/Models/Students.cs
@@ -1,34 +1,51 @@
-﻿namespace StudentsList.DataModels {
-    public class Student {
-        
+﻿namespace StudentsList.DataModels
+{
+    public class Student
+    {
+        // Backing Member
         private string firstName;
 
+        /// <summary>
+        /// The First name of the student
+        /// </summary>
         public string FirstName
         {
 
             get { return firstName; }
             set
             {
+                // Set the first name to the new value
                 firstName = value;
-                FullName = firstName + LastName;
+                // Update the Full name property
+                FullName = string.Format("{0} {1}", firstName ,LastName);
             }
         }
 
+        // Backing member
         private string lastName;
 
-        public string LastName {
+        /// <summary>
+        /// The last name of the student
+        /// </summary>
+        public string LastName
+        {
             get { return lastName; }
-            set {
+            set
+            {
+                // Set the last name to the new value
                 lastName = value;
-                FullName = FirstName + lastName;
+                // Update the FullName property with the new last name
+                FullName = string.Format("{0} {1}", FirstName, lastName);
             }
 
-
-
-
         }
 
+        /// <summary>
+        /// The full name of the student (includes a space in between first and last name
+        /// </summary>
         public string FullName { get; set; }
 
-        }
+        public bool ShowDelete { get; set; }
+
     }
+}
--- StudentsList/ViewModels/MainWindowViewModel.cs
+++ StudentsList/ViewModels/MainWindowViewModel.cs
@@ -1,65 +1,135 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using DynamicData;
 using StudentsList.DataModels;
 
 namespace StudentsList.ViewModels;
 
-public partial class MainViewModel : ObservableObject {
+public partial class MainViewModel : ObservableObject
+{
 
-    private ObservableCollection<Student> students = new();
+    #region Public Properties and their backing members
 
+    // Backing Member.  Sets initial value to string.Empty
     private string titleName = string.Empty;
-    public string TitleName {get; set;}
 
+    /// <summary>
+    /// The Text that is displayed in the Title Textblock
+    /// </summary>
+    public string TitleName { get; set; }
+
+    // Backing member
     private string descriptionName = string.Empty;
-    public string DescriptionName { get; set; }
-    
-    // The Student selected in the collection
-    [ObservableProperty]
-    private Student selectedStudent;
-    
-    
-    public ObservableCollection<Student> Students {
-        get {
-            return students;
-        }
-        set {
 
-            SetProperty(ref students, value);
+    /// <summary>
+    /// The Text that is displayed in the Description Textblock
+    /// </summary>
+    public string DescriptionName { get; set; }
+
+    /// <summary>
+    /// The Student selected in the collection of the listbox
+    /// </summary>
+
+    private Student selectedStudent;
+
+    public Student SelectedStudent
+    {
+        get { return selectedStudent; }
+
+        set
+        {
+            selectedStudent = value;
+
+            var temp = new ObservableCollection<Student>();
+
+            foreach(var student in Students)
+            {
+                if (student == selectedStudent)
+                    student.ShowDelete = true;
+
+                temp.Add(student);
+            }
+
+            Students = temp;
+           
         }
     }
 
+    [ObservableProperty]
+    private ObservableCollection<Student> students = new();
+   
+
+    //// Backing member
+    //private ObservableCollection<Student> students = new();
+
+    ///// <summary>
+    ///// The collection of students to display
+    ///// </summary>
+    //public ObservableCollection<Student> Students
+    //{
+    //    get
+    //    {
+    //        return students;
+    //    }
+    //    set
+    //    {
+    //        SetProperty(ref students, value);
+    //    }
+    //}
+
+    #endregion
+
+
+    #region Default Constructor
+
+    public MainViewModel()
+    {
+        Students.Add(new Student() { FirstName = "John", LastName = "MacGyver", ShowDelete = true});
+        Students.Add(new Student() { FirstName = "Jason", LastName = "Statham", ShowDelete = false });
+        Students.Add(new Student() { FirstName = "Francis", LastName = "Ngannou", ShowDelete = false });
+        SetTitle();
+        SetDescription();
+    }
+
+    #endregion
+
+
+    #region Relay Commands
+
+    // Add Student to the TextBox
+    [RelayCommand]
+    public void AddButton()
+    {
+        Students.Add(new Student() { FirstName = "John", LastName = "Doe", ShowDelete = false });
+    }
+
+    // Remove Student from the TextBox
+
+    [RelayCommand]
+    public void RemoveButton()
+    {
+        //Students.Remove(new Student() { FirstName = "John", LastName = "Doe" });
+    }
+
+    #endregion
+
+
+    #region Private methods
+
     // Define the Title : See Line 28 in MainWindow.axaml
-    public void SetTitle() {
+    private void SetTitle()
+    {
         TitleName = "Students List";
     }
 
     // Define a description in the second Border 
-    public void setDescription() {
+    private void SetDescription()
+    {
         DescriptionName = "Go to scholl !!!";
     }
 
-    
-    // Add Student to the TextBox
-    [RelayCommand]
-    public void addButton() {
-        Students.Add(new Student() { FirstName = "John", LastName = "Doe" });
-    }
-    
-    // Remove Student from the TextBox
-
-    [RelayCommand]
-    public void removeButton() {
-        Students.Remove(new Student() { FirstName = "John", LastName = "Doe" });
-    } 
-
-    public MainViewModel() { 
-        Students.Add(new Student() { FirstName = "John", LastName = "MacGyver" });
-        Students.Add(new Student() { FirstName = "Jason", LastName = "Statham" });
-        Students.Add(new Student() { FirstName = "Francis", LastName = "Ngannou" });    
-        SetTitle();
-        setDescription();
-    }
+    #endregion
 
 }
